9b6f40124f4cbb4882fe00b64b6f4ee14e19474a
vivek <vivekgj2019@gmail.com>
2025-09-05 14:01:39 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,25 @@ import cors from 'cors'
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
 import rateLimiter from './middleware/rateLimiter.js';
+import path from 'path'
+
 
 dotenv.config();
 
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
+const __dirname = path.resolve()
 
 
 //middleware
+
+if(process.env.NODE_ENV !== "production"){
 app.use(cors({
     origin:"http://localhost:5173"
 }
 ));
+}
 app.use(express.json());  // this middleware will parse json bodies: req.bodies
 
 app.use(rateLimiter);
@@ -34,6 +39,16 @@ app.use((req,res,next) =>{
 // An endpoint is a combination of URL + HTTP method that lets the client interact with a specific resource
 
 app.use('/api/notes' , notesRoutes);
+
+
+if(process.env.NODE_ENV === "production"){
+        app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+});
+}
+
+
 
 connectDB().then(()=>{
     app.listen(PORT , () =>{
